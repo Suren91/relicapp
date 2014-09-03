43a9598a8d66331d3e5521141164ee5e373c7fbf
Suren91 <surendra.koripella242@gmail.com>
2014-09-03 12:46:18 -0400

    Initial commit

--- app/send.rb
+++ app/send.rb
@@ -2,6 +2,7 @@ require 'rest_client'
 require 'json'
 
 
-h = {:to => "Surendra Koripella <suren8990@gmail.com>", :body => "test body", :subject => "Hello Testing, Relic app interview task"}.to_json 
+h = {"to": "Siva Krishna <sivakr39@yahoo.com>", "subject": "hello world", "body": "Hi Siva! Sending you an email via this API I just made."}.to_json 
+ 
 puts h
 RestClient.post "http://lit-plateau-2174.herokuapp.com/emailit", h , :content_type => :json, :accept => :json
