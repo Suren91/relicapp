4a44680d6beadabe9d14d15997b82205e2c9201b
Suren91 <surendra.koripella242@gmail.com>
2014-09-03 12:32:13 -0400

    Initial commit

--- app/send.rb
+++ app/send.rb
@@ -4,4 +4,4 @@ require 'json'
 
 h = {:to => "Surendra Koripella <suren8990@gmail.com>", :body => "test body", :subject => "Hello Testing, Relic app interview task"}.to_json 
 puts h
-RestClient.post "http://fierce-headland-6366.herokuapp.com/emailit", h , :content_type => :json, :accept => :json
+RestClient.post "http://lit-plateau-2174.herokuapp.com/emailit", h , :content_type => :json, :accept => :json
